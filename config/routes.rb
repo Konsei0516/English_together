6e8c96e227d36af831bb6869707d13075c4068ea
Konsei0516 <masaya19960516@gmail.com>
2020-01-11 20:53:22 +0900

    userの詳細画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "home#top"
   resources :homes, only: [:top,:about]
   resources :rooms, only: [:show,:index]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end
