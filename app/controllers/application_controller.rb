4cf5e83c18fca7fba877b83ed12636036395a6d6
Konsei0516 <masaya19960516@gmail.com>
2020-01-13 17:14:59 +0900

    gem install
    画像をアップロード
    deviseのセキュリティー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   def after_sign_in_path_for(resource)
     '/rooms/:id' # ログイン後に遷移するpathを設定
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
     @nickname = current_user.nickname
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_show_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :remember_digest)
+  end
 end
