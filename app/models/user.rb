f9393d065097467aa0167bdf34a3b77fd7663192
Konsei0516 <masaya19960516@gmail.com>
2020-01-25 11:47:52 +0900

    usernameの表示
    viewの調整
    usernameの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :messages
   validates :name, presence: true
   validates :profile, length: {maximum: 200}
-  has_many :messages
+  
 end
