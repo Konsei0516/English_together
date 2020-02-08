e3d090273edd0cdbfc83877cd0f7d451ceeb1f80
Konsei0516 <masaya19960516@gmail.com>
2020-02-08 12:35:53 +0900

    wordの新規登録
    新規登録画面
    新規登録機能

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,13 +1,22 @@
 class WordsController < ApplicationController
   def index
+    @words = Word.all
   end
 
   def show
   end
 
   def new
+    @word = Word.new(word_params)
+    word.save!
+    redirect_to words_url
   end
 
   def edit
+  end
+
+  private
+  def word_params
+    params.require(:word).permit(:name, :description)
   end
 end
