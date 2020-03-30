757cfc2a76b3ade71100c564e42fba0af87a311f
Konsei0516 <masaya19960516@gmail.com>
2020-03-30 22:01:00 +0900

    ユーザー編集
    ユーザー編集
    画像画面変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,6 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  #process :resize_to_limit => [800, 800]
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
