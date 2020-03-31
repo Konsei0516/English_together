6c0aa146fd9bd83c08d679c1f8f0b1d6f6c422e8
Konsei0516 <masaya19960516@gmail.com>
2020-03-31 16:03:25 +0900

    時間で背景変化
    roomに画像表示
    時間で背景が変化

---
+++ app/assets/javascripts/time_change.js
@@ -0,0 +1,5 @@
+$(function(){
+  var getH = new Date().getHours();
+  if((getH >= 6) && (getH < 17)) { $('.chat_box').removeClass('night').addClass('morning'); }
+  else if( (getH >= 18) || (getH < 5) ) { $('.chat_box').removeClass('morning').addClass('night'); }
+});
