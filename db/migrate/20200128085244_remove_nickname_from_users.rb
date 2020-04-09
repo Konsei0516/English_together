75ac6b4a2983802b3e2d53119402d692d454ddf1
Konsei0516 <masaya19960516@gmail.com>
2020-04-09 17:13:30 +0900

    migrateファイル修正

--- db/migrate/20200128085244_remove_nickname_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNicknameFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :nickname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_115414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
