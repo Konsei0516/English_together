ca50788065f8864a463d76f640c68907e0288eb1
Konsei0516 <masaya19960516@gmail.com>
2020-04-12 10:21:58 +0900

    エラーの修正２
    フロント変更とエラーの修正

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load',function() {
   var i, len, ref, results, tag;
   $('#article-tags').tagit({
     fieldName: 'word[tag_list]',
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @word = @user.words.order(created_at: :desc)
-    @words = Word.order(created_at: :desc).page(params[:page]).per(6)
+    @words = @user.words.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def edit
