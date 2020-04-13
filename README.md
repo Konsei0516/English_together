2155f5fec319a6adaf83d010f99b67f9e6390c15
Konsei0516 <masaya19960516@gmail.com>
2020-04-13 15:25:17 +0900

    viewの修正
    about追加
    細かい修正

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # English_together
 
 ### http://18.180.23.221/
+
+# Englsh togetherとは？
+英語学習用アプリケーションになります。
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
 
+  # 英語のみバリデーション
   VALID_MESSAGE_REGEX = /\A[0-9a-zA-Z]*\z/
 
   validates :content, presence: true,format:{ with: VALID_MESSAGE_REGEX }
