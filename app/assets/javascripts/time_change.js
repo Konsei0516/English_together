c1903af3f33519cee627b015f4a546f262a026ab
Konsei0516 <masaya19960516@gmail.com>
2020-04-16 21:11:57 +0900

    リファクタリング
    テスト準備

--- app/assets/javascripts/time_change.js
+++ app/assets/javascripts/time_change.js
@@ -1,6 +1,6 @@
 //夕方以降の背景変更
 $(document).on('turbolinks:load',function(){
   var getH = new Date().getHours();
-  if((getH >= 6) && (getH < 17)) { $('.chat_box').removeClass('night').addClass('morning'); }
-  else if( (getH >= 18) || (getH < 5) ) { $('.chat_box').removeClass('morning').addClass('night'); }
+  if((getH >= 6) && (getH < 17)) { $('.chat').removeClass('night').addClass('morning'); }
+  else if( (getH >= 18) || (getH < 5) ) { $('.chat').removeClass('morning').addClass('night'); }
 });
