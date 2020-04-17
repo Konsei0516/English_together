4d99c814de537cc700601a8dab5c9b458dedc2d4
Konsei0516 <masaya19960516@gmail.com>
2020-04-17 20:24:11 +0900

    背景の修正
    リファクタリングと修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   # 英語のみバリデーション
-  VALID_MESSAGE_REGEX = /\A[0-9a-zA-Z]*\z/
+  VALID_MESSAGE_REGEX =  /\A[\w` -]*[A-Za-z][\w` -]*\z/
+  
 
   validates :content, presence: true,format:{ with: VALID_MESSAGE_REGEX }
   after_create_commit { MessageBroadcastJob.perform_later self }
